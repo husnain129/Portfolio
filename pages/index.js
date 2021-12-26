da0c7810c616767fe11010909c7c34f136624d9f
Muhammad Husnain <54207663+m-h9@users.noreply.github.com>
2021-12-26 21:18:47 +0500

    update header export

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import About from "./components/About";
-import { Header } from "./components/Header";
+import Header from "./components/Header";
 import Menu from "./components/Menu";
 import Project from "./components/Project";
 import Work from "./components/Work";
